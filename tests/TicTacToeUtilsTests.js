626680b36c5d7856f37d919b28f6d0100567b7e1
Karen Morales <karen.morales@jalasoft.com>
2022-08-04 07:06:07 -0400

    ParaOctopus

--- tests/TicTacToeUtilsTests.js
+++ tests/TicTacToeUtilsTests.js
@@ -1,33 +1,44 @@
-var tictactoeUtils = require('../src/TicTacToeUtils');
+/* eslint-disable no-undef */
+const assert = require('assert');
+const tictactoeUtils = require('../src/TicTacToeUtils');
 
-var assert = require('assert');
-describe("the function", function () {
-  it("works", function () {
-    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    var board = ticTacToeUtils.buildBoard(2);
-    var expectedBoard = {
-        board: [
-            [
-            null,
-            null
-            ],
-            [
-            null,
-            null
-            ]
+describe('Board size two', () => {
+  it('works', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const board = ticTacToeUtils.buildBoard(2);
+    const expectedBoard = {
+      board: [
+        [
+          null,
+          null,
         ],
-        empty: [
-            '0 0',
-            '0 1',
-            '1 0',
-            '1 1'
-        ]
-        };
+        [
+          null,
+          null,
+        ],
+      ],
+      empty: [
+        '0 0',
+        '0 1',
+        '1 0',
+        '1 1',
+      ],
+    };
     assert.notEqual(board, expectedBoard);
   });
 });
 
-var assert = require('assert');
+//<<<<<<< HEAD
+describe('compare tictactoe board size', () => {
+  it('works', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const board = ticTacToeUtils.buildBoard(2);
+    const expectedBoardSize = 2;
+    const currentBoardSize = board.board.length;
+    assert.equal(expectedBoardSize, currentBoardSize);
+  });
+});
+//=======
 describe("compare tictactoe board size", function () {
   it("works", function () {
     var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
@@ -38,4 +49,30 @@ describe("compare tictactoe board size", function () {
   });
 });
 
+//<<<<<<< .merge_file_a06892
 //61one comment//
+//=======
+describe("setup players", function () {
+  it("setupPlayers", function () {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    let currentConfiguration = ticTacToeUtils.setupPlayers(["Rocio", "Pedro"])
+    
+    let containsRocioPlayers = currentConfiguration.find(player => { return player.type === "Rocio"});
+    assert.notEqual(containsRocioPlayers, null);
+
+    let containsPedroPlayers = currentConfiguration.find(player => { return player.type === "Pedro"});
+    assert.notEqual(containsPedroPlayers, null);
+    //Para Ej 8//
+  });
+});
+
+describe("setup more than two players", function () {
+  it("setupPlayers", function () {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    let players = ["Rocio", "Pedro", "Roberto"];
+    let currentConfiguration = ticTacToeUtils.setupPlayers(players)
+    assert.equal(0, currentConfiguration.length);
+  });
+});
+ feature/setupPlayersUnitTests
+ .merge_file_a21176
