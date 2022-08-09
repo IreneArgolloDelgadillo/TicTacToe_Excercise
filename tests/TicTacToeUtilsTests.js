46d0154b82351cca97939b8d95bc8b19ae476191
Irene Argollo <ireneirene892@gmail.com>
2022-08-09 09:19:13 -0400

    Fix eslint rules

--- tests/TicTacToeUtilsTests.js
+++ tests/TicTacToeUtilsTests.js
@@ -3,100 +3,100 @@
 const assert = require('assert');
 const tictactoeUtils = require('../src/TicTacToeUtils');
 
-describe("Board size two", function () {
-  it("buildBoard", function () {
-    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    var board = ticTacToeUtils.buildBoard(2);
-    var expectedBoard = {
-        board: [
-            [
-            null,
-            null
-            ],
-            [
-            null,
-            null
-            ]
+describe('Board size two', () => {
+  it('buildBoard', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const board = ticTacToeUtils.buildBoard(2);
+    const expectedBoard = {
+      board: [
+        [
+          null,
+          null,
         ],
-        empty: [
-            '0 0',
-            '0 1',
-            '1 0',
-            '1 1'
-        ]
-        };
+        [
+          null,
+          null,
+        ],
+      ],
+      empty: [
+        '0 0',
+        '0 1',
+        '1 0',
+        '1 1',
+      ],
+    };
     assert.notEqual(board, expectedBoard);
   });
 });
 
-describe("compare tictactoe board size", function () {
-  it("buildBoard", function () {
-    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    var board = ticTacToeUtils.buildBoard(2);
-        var expectedBoardSize = 2;
-        let currentBoardSize = board.board.length;
+describe('compare tictactoe board size', () => {
+  it('buildBoard', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const board = ticTacToeUtils.buildBoard(2);
+    const expectedBoardSize = 2;
+    const currentBoardSize = board.board.length;
     assert.equal(expectedBoardSize, currentBoardSize);
   });
 });
 
-describe("Build Empty Board", function () {
-  it("buildBoard", function () {
-    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    var expectedBoardSize = 0;
-    var board = ticTacToeUtils.buildBoard(expectedBoardSize);
-    let currentBoardSize = board.board.length;
+describe('Build Empty Board', () => {
+  it('buildBoard', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const expectedBoardSize = 0;
+    const board = ticTacToeUtils.buildBoard(expectedBoardSize);
+    const currentBoardSize = board.board.length;
     assert.equal(expectedBoardSize, currentBoardSize);
   });
 });
-describe("setup players", function () {
-  it("setupPlayers", function () {
+describe('setup players', () => {
+  it('setupPlayers', () => {
     const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    let currentConfiguration = ticTacToeUtils.setupPlayers(["Rocio", "Pedro"])
-    
-    let containsRocioPlayers = currentConfiguration.find(player => { return player.type === "Rocio"});
+    const currentConfiguration = ticTacToeUtils.setupPlayers(['Rocio', 'Pedro']);
+
+    const containsRocioPlayers = currentConfiguration.find((player) => player.type === 'Rocio');
     assert.notEqual(containsRocioPlayers, null);
 
-    let containsPedroPlayers = currentConfiguration.find(player => { return player.type === "Pedro"});
+    const containsPedroPlayers = currentConfiguration.find((player) => player.type === 'Pedro');
     assert.notEqual(containsPedroPlayers, null);
   });
 });
 
-describe("setup more than two players", function () {
-  it("setupPlayers", function () {
+describe('setup more than two players', () => {
+  it('setupPlayers', () => {
     const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    let players = ["Rocio", "Pedro", "Roberto"];
-    let currentConfiguration = ticTacToeUtils.setupPlayers(players)
+    const players = ['Rocio', 'Pedro', 'Roberto'];
+    const currentConfiguration = ticTacToeUtils.setupPlayers(players);
     assert.equal(0, currentConfiguration.length);
   });
 });
-describe("setup players", function () {
-  it("setupPlayers", function () {
+describe('setup players', () => {
+  it('setupPlayers', () => {
     const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    let currentConfiguration = ticTacToeUtils.setupPlayers(["Rocio", "Pedro"])
-    
-    let containsRocioPlayers = currentConfiguration.find(player => { return player.type === "Rocio"});
+    const currentConfiguration = ticTacToeUtils.setupPlayers(['Rocio', 'Pedro']);
+
+    const containsRocioPlayers = currentConfiguration.find((player) => player.type === 'Rocio');
     assert.notEqual(containsRocioPlayers, null);
 
-    let containsPedroPlayers = currentConfiguration.find(player => { return player.type === "Pedro"});
+    const containsPedroPlayers = currentConfiguration.find((player) => player.type === 'Pedro');
     assert.notEqual(containsPedroPlayers, null);
   });
 });
 
-describe("setup more than two players", function () {
-  it("setupPlayers", function () {
+describe('setup more than two players', () => {
+  it('setupPlayers', () => {
     const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    let players = ["Rocio", "Pedro", "Roberto"];
-    let currentConfiguration = ticTacToeUtils.setupPlayers(players)
+    const players = ['Rocio', 'Pedro', 'Roberto'];
+    const currentConfiguration = ticTacToeUtils.setupPlayers(players);
     assert.equal(0, currentConfiguration.length);
   });
 });
 
-describe("Build three size Board", function () {
-  it("buildBoard", function () {
-    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
-    var expectedBoardSize = 3;
-    var board = ticTacToeUtils.buildBoard(expectedBoardSize);
-    let currentBoardSize = board.board[0].length;
+describe('Build three size Board', () => {
+  it('buildBoard', () => {
+    const ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    const expectedBoardSize = 3;
+    const board = ticTacToeUtils.buildBoard(expectedBoardSize);
+    const currentBoardSize = board.board[0].length;
     assert.equal(expectedBoardSize, currentBoardSize);
   });
 });
